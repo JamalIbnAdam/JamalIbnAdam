4f00d48cd736a748e52ff2101aec83b38e9886cc
JamalIbnAdam <jamalibnadam@gmail.com>
2026-02-09 21:44:17 +0200

    Merge branch 'main' of https://github.com/JamalIbnAdam/JamalIbnAdam

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jamalibnadam-v1';
+const CACHE_NAME = 'jamalibnadam-v2';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
@@ -15,12 +15,30 @@ const ASSETS_TO_CACHE = [
 ];
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then((cache) => {
-                return cache.addAll(ASSETS_TO_CACHE);
+                return cache.addAll(ASSETS_TO_CACHE).catch((err) => {
+                    console.warn('Some assets failed to cache:', err);
+                });
             })
     );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    return self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
